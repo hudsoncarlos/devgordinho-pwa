8c8c0a6a8c484d3bf3b876884a5efdd59c3d9aea
Hudson S Carlos <hudsonscarlos@outlook.com>
2022-03-24 23:52:43 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
-window.addEventListener('load', ()=>{
-    registerSW()
-})
+window.addEventListener("load", () => {
+  //location.reload();
+  registerSW();
+  console.log("Terminou de carregar.");
+});
 
-async function registerSW(){
-    if('serviceWorker' in navigator){
-        try{
-            await navigator.serviceWorker.register('./js/sw.js')
-        } catch(e){
-            console.log(`SW registration failed`);
-        }
-    }
+async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    await navigator.serviceWorker
+      .register("sw.js")
+      .then(function (reg) {
+        console.log(
+          "[PWA Builder] - Service worker foi registrado para escopo: " +
+            reg.scope
+        );
+      })
+      .catch(function (error) {
+        console.log(
+          "[PWA Builder] ativo encontrado, não há necessidade de se registrar" +
+            error
+        );
+      });
+  }
 }
---
+++ sw.js
@@ -0,0 +1,114 @@
+const version = "0.0.1";
+const cacheName = `ghlm-${version}`;
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache
+        .addAll([
+          "/",
+          "/css/",
+          "/css/index.css",
+          "/getmdl/",
+          "/getmdl/bower.json",
+          "/getmdl/material.css",
+          "/getmdl/material.js",
+          "/getmdl/material.min.css",
+          "/getmdl/material.min.css.map",
+          "/getmdl/material.min.js",
+          "/getmdl/material.min.js.map",
+          "/getmdl/package.json",
+          "/img/",
+          "/img/blender/",
+          "/img/blender/blender_black_1x.png",
+          "/img/blender/blender_black_2x.png",
+          "/img/blender/blender_white_1x.png",
+          "/img/blender/blender_white_2x.png",
+          "/img/email/",
+          "/img/email/email_black_1x.png",
+          "/img/email/email_black_2x.png",
+          "/img/email/email_white_1x.png",
+          "/img/email/email_white_2x.png",
+          "/img/faces_happy/",
+          "/img/faces_happy/faces_happy_black_1x.png",
+          "/img/faces_happy/faces_happy_black_2x.png",
+          "/img/faces_happy/faces_happy_white_1x.png",
+          "/img/faces_happy/faces_happy_white_2x.png",
+          "/img/image/",
+          "/img/image/image_black_1x.png",
+          "/img/image/image_black_2x.png",
+          "/img/image/image_white_1x.png",
+          "/img/image/image_white_2x.png",
+          "/img/logo/",
+          "/img/logo/logo_360x360.png",
+          "/img/menu/",
+          "/img/menu/menu_black_1x.png",
+          "/img/menu/menu_black_2x.png",
+          "/img/menu/menu_white_1x.png",
+          "/img/menu/menu_white_2x.png",
+          "/img/search/",
+          "/img/search/search_black_1x.png",
+          "/img/search/search_black_2x.png",
+          "/img/search/search_white_1x.png",
+          "/img/search/search_white_2x.png",
+          "/js/",
+          "/js/index.js",
+          "/manifest.json",
+          "/sw.js",
+        ])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.open(cacheName)
+//       .then(cache => cache.match(event.request, {ignoreSearch: true}))
+//       .then(response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+// self.addEventListener("refreshOffline", function () {
+//   const offlinePageRequest = new Request();
+// });
+
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  console.log('url -' + url.login + ' - ')
+  console.log('origin -' + location.origin)
+
+  if (url.login === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  console.log('cacheFirst - ' + req)
+
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const refresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return refresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
